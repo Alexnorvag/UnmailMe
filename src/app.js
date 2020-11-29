6812eef1e87c03e8175e276fe6da8e438f387f8e
Alexnorvag <norvag@mail.ru>
2020-11-30 00:00:46 +0300

    app: configured project structure

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Navigation</Text>
+      <Text>Redux</Text>
+      <Text>Axios Api</Text>
+    </View>
+  );
+};
+
+export default App;
